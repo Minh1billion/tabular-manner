d593075654a9ac93a56b1b73f2ac3ee1c04c0d3f
= <=>
2026-08-22 20:59:18 +0700

    tests(sandbox): add adversarial cases for dunder, exec/eval, comprehension, and syntax-level sandbox escapes

--- tests/unit/application/runtime/test_sandbox.py
+++ tests/unit/application/runtime/test_sandbox.py
@@ -43,6 +43,64 @@ class TestSandboxBlocksArbitraryPlCalls:
 
         assert not target.exists()
 
+class TestSandboxAdversarial:
+    @pytest.mark.parametrize(
+        "expression",
+        [
+            "__import__('os').system('id')",
+            "().__class__.__bases__[0].__subclasses__()",
+            "df.__class__",
+            "df.amount.__class__.__bases__",
+            "getattr(df, '__class__')",
+            "setattr(df, 'x', 1)",
+            "(1).__class__",
+            "().__class__",
+            "df.amount.__init__.__globals__",
+            "(lambda: 1)()",
+            "(lambda x: x.__class__)(df)",
+            "exec('1')",
+            "eval('1')",
+            "compile('1', '<s>', 'eval')",
+            "[x for x in range(3)]",
+            "{x for x in range(3)}",
+            "{x: x for x in range(3)}",
+            "(x for x in range(3))",
+            "1 if True else 2",
+            "(x := 1)",
+            "df['amount']",
+            "df.amount[0]",
+            "f'{df}'",
+            "import os",
+            "globals()",
+            "locals()",
+            "vars(df)",
+            "type(df)",
+            "df.amount.__doc__",
+            "pl.__loader__",
+            "pl.__builtins__",
+            "df.amount.__reduce__()",
+            "df.amount.__subclasshook__",
+            "yield 1",
+            "assert False",
+            "del df",
+        ],
+    )
+    def test_blocked(self, sandbox, expression):
+        with pytest.raises(ValueError):
+            sandbox.check_expression(expression)
+
+    def test_blocked_dunder_attribute_regardless_of_base(self, sandbox):
+        with pytest.raises(ValueError):
+            sandbox.check_expression("pl.col('a')._private_field")
+
+    def test_blocked_free_function_call_without_attribute(self, sandbox):
+        with pytest.raises(ValueError):
+            sandbox.check_expression("str(df)")
+
+    def test_allowed_names_are_enforced_even_for_arbitrary_identifiers(self, sandbox):
+        with pytest.raises(ValueError):
+            sandbox.check_expression("os", allowed_names=frozenset({"df", "pl"}))
+
 class TestSandboxAllowsLegitimateExpressions:
     @pytest.mark.parametrize(
         "expression",
